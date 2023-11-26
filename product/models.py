c39352392dc48079ad8ef36c8d73fa82e20ab388
lemonada02 <jrlimon02@gmail.com>
2023-11-26 18:25:01 +0100

    fix: arreglo vista catalogo

--- product/models.py
+++ product/models.py
@@ -8,7 +8,36 @@ class Producto(models.Model):
     imagen = models.CharField(max_length=255)
     precio = models.FloatField()
     descripcion = models.CharField(max_length=255)
-    departamento = models.CharField(max_length=255)
-    seccion = models.CharField(max_length=255)
-    fabricante = models.CharField(max_length=255)
+    departamento_choices= [
+        ("Piezas", "Piezas"),
+        ("Drones", "Drones"),
+        ("Accesorios", "Accesorios"),
+    ]
+    departamento = models.CharField(max_length=255, choices= departamento_choices,)
+    seccion_choices= [
+        ("Electronico", "Electronico"),
+        ("No-Electronico", "No-Electronico"),
+        ("Carga", "Carga"),
+        ("Vigilancia", "Vigilancia"),
+        ("Fotografia", "Fotografia"),
+        ("Videografia", "Videografia"),
+        ("Cuadricoptero", "Cuadricoptero"),
+        ("Hexacoptero", "Hexacoptero"),
+        ("Agricola", "Agricola"),
+        ("Recopilador", "Recopilador"),
+        ("Envio", "Envio"),
+    ]
+    seccion = models.CharField(max_length=255, choices= seccion_choices,)
+    fabricante_choices= [
+        ("DJI", "DJI"),
+        ("Parrot", "Parrot"),
+        ("Yuneec", "Yuneec"),
+        ("Autel", "Autel"),
+        ("3DR", "3DR"),
+        ("Skydio", "Skydio"),
+        ("Walkera", "Walkera"),
+        ("PowerVision", "PowerVision"),
+        ("Hubsan", "Hubsan"),
+    ]
+    fabricante = models.CharField(max_length=255, choices= fabricante_choices,)
     stock = models.IntegerField()
