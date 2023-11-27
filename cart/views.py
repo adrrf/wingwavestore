1550e5364000537f72a83d0d6408fb81126d9893
adrf <adrromflo@alum.us.es>
2023-11-27 18:35:05 +0100

    fix: fixes in stock logic

--- cart/views.py
+++ cart/views.py
@@ -3,7 +3,7 @@ from order.models import Pedido
 from cart.models import ProductoPedido
 from product.models import Producto
 from user.models import Cliente
-
+from django.contrib import messages
 # Create your views here.
 def carrito(request):
     try:
@@ -17,7 +17,7 @@ def carrito(request):
         cliente, created = Cliente.objects.get_or_create(dispositivo=device)
     order, created = Pedido.objects.get_or_create(cliente=cliente, completado=False)
     if order.productopedido_set.all().count() == 0:
-        return redirect('catalogo')
+        messages.info(request, 'No hay productos en el carrito')
     items = order.productopedido_set.all()
     total = 0
     for item in items:
@@ -29,10 +29,17 @@ def carrito(request):
         envio = 4.99
         order.precio_envio = envio
     order.save()
-    context = {'items': items, 'order': order, 'total': total, 'envio': envio, 'total_envio': total + envio}
+    checkout = True
+    for item in items:
+        if item.producto.stock < item.cantidad:
+            checkout = False
+    context = {'items': items, 'order': order, 'total': total, 'envio': envio, 'total_envio': total + envio, 'checkout': checkout}
     return render(request, 'cart/carrito.html', context)
+
 def addCarrito(request, producto_id):
     producto = Producto.objects.get(id=producto_id)
+    if producto.stock == 0:
+        return redirect('cart')
     try:
         user = request.user
         cliente, created = Cliente.objects.get_or_create(user=user)
@@ -57,6 +64,9 @@ def addCarrito(request, producto_id):
 
 def sumCarrito(request, producto_id):
     producto = Producto.objects.get(id=producto_id)
+    if producto.stock == 0:
+        messages.warning(request, 'No hay stock disponible')
+        return redirect('cart')
     try:
         user = request.user
         cliente, created = Cliente.objects.get_or_create(user=user)
@@ -68,12 +78,18 @@ def sumCarrito(request, producto_id):
         cliente, created = Cliente.objects.get_or_create(dispositivo=device)
     order, created = Pedido.objects.get_or_create(cliente=cliente, completado=False)
     orderItem, created = ProductoPedido.objects.get_or_create(producto=producto, pedido=order)
+    if orderItem.cantidad == producto.stock:
+        messages.warning(request, 'No hay stock disponible')
+        return redirect('cart')
     orderItem.cantidad = orderItem.cantidad + 1
     orderItem.save()
     return redirect('cart')
 
 def restCarrito(request, producto_id):
     producto = Producto.objects.get(id=producto_id)
+    if producto.stock == 0:
+        messages.warning(request, 'No hay stock disponible')
+        return redirect('cart')
     try:
         user = request.user
         cliente, created = Cliente.objects.get_or_create(user=user)
@@ -85,6 +101,9 @@ def restCarrito(request, producto_id):
         cliente, created = Cliente.objects.get_or_create(dispositivo=device)
     order, created = Pedido.objects.get_or_create(cliente=cliente, completado=False)
     orderItem, created = ProductoPedido.objects.get_or_create(producto=producto, pedido=order)
+    if created:
+        orderItem.delete()
+        return redirect('cart')
     if orderItem.cantidad > 1:
         orderItem.cantidad = orderItem.cantidad - 1
         orderItem.save()
