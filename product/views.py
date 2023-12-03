beddc09cb4878b71f31e1c9af0e061207f462460
lemonada02 <jrlimon02@gmail.com>
2023-12-03 18:04:15 +0100

    fix: buscador actualizado

--- product/views.py
+++ product/views.py
@@ -7,104 +7,155 @@ from django.db.models import Q
 def catalogo(request):
     products = Producto.objects.all()
     choices_fabricante = Producto.fabricante.field.choices
+    choices_seccion = Producto.seccion.field.choices
     lista_fabricante = []
+    lista_seccion = []
     for c in choices_fabricante:
         lista_fabricante.append(c[1])
 
+    for c in choices_seccion:
+        lista_seccion.append(c[1])
+
     busqueda = request.GET.get('buscar')
     marca = request.GET.get('marca')
+    seccion = request.GET.get('seccion')
     if busqueda:
-        products = Producto.objects.filter(
+        products= products.filter(
             Q(nombre__icontains=busqueda)
         ).distinct()
-    elif marca:
-        products = Producto.objects.filter(
+    if marca:
+        products = products.filter(
             Q(fabricante__icontains=marca)
         ).distinct()
+    if seccion:
+        products = products.filter(
+            Q(seccion__icontains=seccion)
+        ).distinct()
     
-    context = {'products': products, 'choices_fabricante': lista_fabricante} 
+    context = {'products': products, 'choices_fabricante': lista_fabricante, 'choices_seccion': lista_seccion} 
     return render(request, 'product/catalogo.html', context)
 
 def accesorios(request):
     products = Producto.objects.all()
     choices_fabricante = Producto.fabricante.field.choices
+    choices_seccion = Producto.seccion.field.choices
     lista_fabricante = []
+    lista_seccion = []
     for c in choices_fabricante:
         lista_fabricante.append(c[1])
 
+    for c in choices_seccion:
+        lista_seccion.append(c[1])
+
     busqueda = request.GET.get('buscar')
     marca = request.GET.get('marca')
+    seccion = request.GET.get('seccion')
     if busqueda:
-        products = Producto.objects.filter(
+        products= products.filter(
             Q(nombre__icontains=busqueda)
         ).distinct()
-    elif marca:
-        products = Producto.objects.filter(
+    if marca:
+        products = products.filter(
             Q(fabricante__icontains=marca)
         ).distinct()
-
-    context = {'products': products, 'choices_fabricante': lista_fabricante}    
+    if seccion:
+        products = products.filter(
+            Q(seccion__icontains=seccion)
+        ).distinct()
+    
+    context = {'products': products, 'choices_fabricante': lista_fabricante, 'choices_seccion': lista_seccion}     
     return render(request, 'product/accesorios.html', context)
 
 def drones(request):
     products = Producto.objects.all()
     choices_fabricante = Producto.fabricante.field.choices
+    choices_seccion = Producto.seccion.field.choices
     lista_fabricante = []
+    lista_seccion = []
     for c in choices_fabricante:
         lista_fabricante.append(c[1])
 
+    for c in choices_seccion:
+        lista_seccion.append(c[1])
+
     busqueda = request.GET.get('buscar')
     marca = request.GET.get('marca')
+    seccion = request.GET.get('seccion')
     if busqueda:
-        products = Producto.objects.filter(
+        products= products.filter(
             Q(nombre__icontains=busqueda)
         ).distinct()
-    elif marca:
-        products = Producto.objects.filter(
+    if marca:
+        products = products.filter(
             Q(fabricante__icontains=marca)
         ).distinct()
-
-    context = {'products': products, 'choices_fabricante': lista_fabricante}
+    if seccion:
+        products = products.filter(
+            Q(seccion__icontains=seccion)
+        ).distinct()
+    
+    context = {'products': products, 'choices_fabricante': lista_fabricante, 'choices_seccion': lista_seccion} 
     return render(request, 'product/drones.html', context)
 
 def piezas(request):
     products = Producto.objects.all()
     choices_fabricante = Producto.fabricante.field.choices
+    choices_seccion = Producto.seccion.field.choices
     lista_fabricante = []
+    lista_seccion = []
     for c in choices_fabricante:
         lista_fabricante.append(c[1])
 
+    for c in choices_seccion:
+        lista_seccion.append(c[1])
+
     busqueda = request.GET.get('buscar')
     marca = request.GET.get('marca')
+    seccion = request.GET.get('seccion')
     if busqueda:
-        products = Producto.objects.filter(
+        products= products.filter(
             Q(nombre__icontains=busqueda)
         ).distinct()
-    elif marca:
-        products = Producto.objects.filter(
+    if marca:
+        products = products.filter(
             Q(fabricante__icontains=marca)
         ).distinct()
-
-    context = {'products': products, 'choices_fabricante': lista_fabricante}    
+    if seccion:
+        products = products.filter(
+            Q(seccion__icontains=seccion)
+        ).distinct()
+    
+    context = {'products': products, 'choices_fabricante': lista_fabricante, 'choices_seccion': lista_seccion} 
     return render(request, 'product/piezas.html', context)
         
 def vista_producto(request, producto_id):
     producto = Producto.objects.get(id=producto_id)
+    products = Producto.objects.all()
     choices_fabricante = Producto.fabricante.field.choices
+    choices_seccion = Producto.seccion.field.choices
     lista_fabricante = []
+    lista_seccion = []
     for c in choices_fabricante:
         lista_fabricante.append(c[1])
 
+    for c in choices_seccion:
+        lista_seccion.append(c[1])
+
     busqueda = request.GET.get('buscar')
     marca = request.GET.get('marca')
+    seccion = request.GET.get('seccion')
     if busqueda:
-        products = Producto.objects.filter(
+        products = products.filter(
             Q(nombre__icontains=busqueda)
         ).distinct()
-    elif marca:
-        products = Producto.objects.filter(
+    if marca:
+        products = products.filter(
             Q(fabricante__icontains=marca)
         ).distinct()
+    if seccion:
+        products = products.filter(
+            Q(seccion__icontains=seccion)
+        ).distinct()
     
-    context = {'producto': producto, 'choices_fabricante': lista_fabricante}
+    context = {'producto': producto, 'choices_fabricante': lista_fabricante, 'choices_seccion': lista_seccion, 'products': products}
     return render(request, 'product/producto.html', context)
--- src/views.py
+++ src/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from product.models import Producto
+from django.db.models import Q
 
 def home(request):
     return render(request, 'index.html')
+
+def busqueda(request):
+    products = Producto.objects.all()
+    busqueda = request.GET.get('buscar')
+    if busqueda:
+        products= products.filter(
+            Q(nombre__icontains=busqueda)
+        ).distinct()
+    context = {'products': products}
+    return render(request, 'product/catalogo.html', context)
