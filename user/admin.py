28981ed52b49ba4319c6f084f8b4436b1dfd6d73
adrf <adrromflo@alum.us.es>
2023-12-04 15:47:15 +0100

    fix: admin reclamacion

--- user/admin.py
+++ user/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
-from user.models import Datos_envio
+from user.models import Datos_envio, Reclamacion, MensajeReclamacion
 
 class Datos_envioAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(Datos_envio, Datos_envioAdmin)
+
+class MensajeReclamacionInline(admin.TabularInline):
+    model = MensajeReclamacion
+
+class ReclamacionAdmin(admin.ModelAdmin):
+    inlines = (MensajeReclamacionInline,)
+    list_display = ('id', 'fecha', 'estado')
+    list_filter = ('estado',)
+    search_fields = ('id',)
+    ordering = ('-fecha',)
+    readonly_fields = ('fecha',)
+
+admin.site.register(Reclamacion, ReclamacionAdmin)
