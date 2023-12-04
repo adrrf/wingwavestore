85ded6f0e92134ecd80446bc771436f6d335032c
adrf <adrromflo@alum.us.es>
2023-12-04 18:30:21 +0100

    fix: typo in search

--- order/views.py
+++ order/views.py
@@ -157,7 +157,7 @@ def search(request):
     if request.method == 'POST':
         order_id = request.POST['order_id']
         try:
-            order = Pedido.objects.get(id=order_id, competado=True)
+            order = Pedido.objects.get(id=order_id, completado=True)
             if not order.completado:
                 messages.error(request, 'El pedido no ha sido completado')
             items = order.productopedido_set.all()
